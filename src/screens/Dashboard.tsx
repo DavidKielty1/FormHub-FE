57546fa7405df356c7faae6c5b86cb23610f5180
David Kielty <davidkielty@live.co.uk>
2024-01-23 11:19:31 +0000

    Added Datagrid Table with dyanmic columns and field valueGetter variables

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,33 +1,47 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { GET_ALL_SUBMISSIONS } from "../graphql/queries/GetSubmissions";
+import { GetSubmissionsQuery } from "../gql/graphql";
+import { startCase, uniq } from "lodash";
 
 function Dashboard() {
-  const { data, loading } = useQuery(gql`
-    query SubmissionsQuery {
-      submissions {
-        id
-        submittedAt
-        data
-      }
-    }
-  `);
+  const { data, loading, error } =
+    useQuery<GetSubmissionsQuery>(GET_ALL_SUBMISSIONS);
 
   if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  if (!data) return;
+
+  const submissions = data.submissions;
+
+  const columns: GridColDef[] = [
+    {
+      field: "id",
+      headerName: "ID",
+      width: 200,
+    },
+    {
+      field: "submittedAt",
+      headerName: "Submitted",
+      width: 200,
+    },
+    ...uniq(submissions.flatMap((s) => Object.keys(s.data))).map((field) => ({
+      field,
+      headerName: startCase(field),
+      width: 200,
+      valueGetter: (params: GridValueGetterParams) => params.row.data[field],
+    })),
+  ];
 
   return (
     <div>
       <DataGrid
-        rows={rows}
+        rows={submissions}
         columns={columns}
-        // initialState={{
-        //   pagination: {
-        //     paginationModel: {
-        //       pageSize: 5,
-        //     },
-        //   },
-        // }}
-        // pageSizeOptions={[5]}
-        disableRowSelectionOnClick
+        sx={{
+          color: "white",
+        }}
       />
     </div>
   );
